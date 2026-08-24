82a9b0ed7256596f5a2f7b9d373a77c55f7947f7
memo figueredo <memof80@gmail.com>
2026-08-24 18:47:06 -0500

    I added a relationship from many to one and many to many, and a method to support ORM's object and not only a dictionaries

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from fastapi import FastAPI, Query, Body, HTTPException, Path, status, Depends
 from pydantic import BaseModel, Field, field_validator, EmailStr, ConfigDict
 from typing import Optional, List, Union, Literal
 from math import ceil
-from sqlalchemy import create_engine, Integer, String, DateTime, Text, select, func
-from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import create_engine, Integer, String, DateTime, Text, select, func, UniqueConstraint, ForeignKey, Table, Column
+from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 DATABASE_URL = os.getenv("DATABASE_URL","sqlite:///./blog.db")
 print("conectado a: ", DATABASE_URL)
@@ -26,13 +26,55 @@ SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, clas
 class Base(DeclarativeBase):
     pass
 
+post_tags = Table(
+    "post_tags",# nombre de la tabla
+    Base.metadata,
+    Column("post_id", ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True),
+    Column("tag_id", ForeignKey("tags.id", ondelete="CASCADE"), primary_key=True)
+)
+
+class AuthorORM(Base):
+    __tablename__ = "authors"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, index=True)
+
+    #creamos la relación de uno a muchos
+    posts: Mapped[List["PostORM"]] = relationship(back_populates="author")# este es el nombre con el cual se relaciona con los Post
+
+class TagORM(Base):
+    __tablename__="tags"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, primary_key=True, index=True)
+    
+    posts: Mapped[List["PostORM"]] = relationship(
+        secondary=post_tags, #cual es la tabla con la cual se va a enlazar o la referencia
+        back_populates="tags",#como se va a acceder a las tags
+        lazy="selectin"
+    )
+
 class PostORM(Base):
     __tablename__ = "posts"# Este es el nombre de la tabla en la base de datos
+    __table_args__ = (UniqueConstraint("title", name="unique_post_title"),) # esto hace que el título del post sea único, es decir, que no se puedan crear dos posts con el mismo título
+    
     #Los siguientes son los atributos de la tabla
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     title: Mapped[str] = mapped_column(String(100), nullable=False, index=True)
     content: Mapped[str] = mapped_column(Text, nullable=False)
     create_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    
+    #informamos a los post la relación con la clase Author
+    author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("authors_id"))#esta relacionando la llave primaria con la foranea como en sql
+    author: Mapped[Optional[int]] = relationship(back_populates="posts") #esta es la relacion de 1 a muchos, 1 author tiene muchos posts
+    
+    tags: Mapped[List["TagORM"]] = relationship(
+        secondary=post_tags,# aqui le decimos que queremos que tags sea relacionado con la tabla intermedia que es post_tags
+        back_populates="posts",# aqui se ele dice cómo va a acceder los tags a los posts
+        lazy="selectin", # la búsqueda se va a hacer con selectin
+        passive_deletes=True# esto es para respetar el ON DELETE CASCADE
+    )
     
 Base.metadata.create_all(bind=engine) # crea la tabla en la base de datos si no existe, esto es para etapa de desarrollo porque para producción se van a crear migraciones
 
@@ -52,10 +94,14 @@ class Tag(BaseModel):
                       max_length=30,
                       description="Nombre de la etiqueta"
                       )
+    
+    model_config = ConfigDict(from_attributes=True)# esto significa que esta clase tambien a¡va a aceptar objetos ORM porque si no, solo recibiria dictionarios y si no hay dictionarios, no sabe que hacer
 
 class Author(BaseModel):
     name: str
     email: EmailStr
+    
+    model_config = ConfigDict(from_attributes=True)
 
 #CAMPOS OPCIONALES POR DEFECTO
 class PostBase(BaseModel):
@@ -63,6 +109,8 @@ class PostBase(BaseModel):
     content: str
     tag: Optional[List[Tag]] = Field(default_factory=list) # esto crea un array [] vacía por cada objeto en el programa
     author: Optional[Author] = None
+    
+    model_config = ConfigDict(from_attributes=True)
   
   
 ##VALIDACIONES FIELD Y AVANZADAS   
@@ -296,6 +344,9 @@ def create_post(post: PostCreate, db: Session = Depends(get_db)):
        #3. se deben traer los valores generados por la base de datos, como el id y la fecha de creación
        db.refresh(new_post)
        return new_post
+   except IntegrityError:
+       db.rollback() # en caso de error, se debe hacer rollback para deshacer la inserción u operación que se estaba haciendo
+       raise HTTPException(status_code=409, detail="El título del post ya existe, prueba con otro")
    except SQLAlchemyError:
        db.rollback() # en caso de error, se debe hacer rollback para deshacer la inserción u operación que se estaba haciendo
        raise HTTPException(status_code=500, detail="Error al crear el post")
